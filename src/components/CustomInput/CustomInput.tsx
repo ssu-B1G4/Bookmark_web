7a4627b90d6fc35e9039ac85b539b20ab3b4772d
solmee <kii5541@naver.com>
2024-10-22 23:28:46 +0900

    refactor: CustomInput props 수정

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,15 +1,21 @@
 import { Input, TextArea } from './CustomInput.style';
 
-interface InputProps {
+interface SmallInputProps {
   placeholder: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const SmallInput = ({ placeholder, value, onChange }: InputProps) => {
+interface LargeTextAreaProps {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const SmallInput = ({ placeholder, value, onChange }: SmallInputProps) => {
   return <Input placeholder={placeholder} value={value} onChange={onChange} />;
 };
 
-export const LargeTextArea = ({ placeholder, value, onChange }: InputProps) => {
+export const LargeTextArea = ({ placeholder, value, onChange }: LargeTextAreaProps) => {
   return <TextArea placeholder={placeholder} value={value} onChange={onChange} />;
 };
