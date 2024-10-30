8eb0b3bc00dd55c89d86ef4bcaf60d865f2a0ed1
solmee <kii5541@naver.com>
2024-10-31 05:06:41 +0900

    refactor: CustomInput 컴포넌트에 ref를 전달받도록 수정
    - react-hook-form을 사용할 예정이라 수정함

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,21 +1,34 @@
+import { forwardRef } from 'react';
+
 import { Input, TextArea } from './CustomInput.style';
 
-interface SmallInputProps {
+interface SmallInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-interface LargeTextAreaProps {
+interface LargeTextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   placeholder: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export const SmallInput = ({ placeholder, value, onChange }: SmallInputProps) => {
-  return <Input placeholder={placeholder} value={value} onChange={onChange} />;
-};
+export const SmallInput = forwardRef<HTMLInputElement, SmallInputProps>(
+  ({ placeholder, value, onChange, ...props }, ref) => {
+    return (
+      <Input ref={ref} placeholder={placeholder} value={value} onChange={onChange} {...props} />
+    );
+  }
+);
 
-export const LargeTextArea = ({ placeholder, value, onChange }: LargeTextAreaProps) => {
-  return <TextArea placeholder={placeholder} value={value} onChange={onChange} />;
-};
+export const LargeTextArea = forwardRef<HTMLTextAreaElement, LargeTextAreaProps>(
+  ({ placeholder, value, onChange, ...props }, ref) => {
+    return (
+      <TextArea ref={ref} placeholder={placeholder} value={value} onChange={onChange} {...props} />
+    );
+  }
+);
+
+SmallInput.displayName = 'SmallInput';
+LargeTextArea.displayName = 'LargeTextArea';
