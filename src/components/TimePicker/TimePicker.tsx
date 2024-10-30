e59fc3daa9557d18a14534ac5c08d6c3ff048b91
solmee <kii5541@naver.com>
2024-10-31 04:27:35 +0900

    fix: TimePicker에 상태를 밖으로 내보내는 부분 추가

--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
-
 import { TimeInput, TimeLabel, TimeWrapper } from './TimePicker.style';
 
-export const TimePicker = () => {
-  const [startTime, setStartTime] = useState('00:00');
-  const [endTime, setEndTime] = useState('00:00');
+interface TimePickerProps {
+  startTime: string;
+  endTime: string;
+  onStartTimeChange: (time: string) => void;
+  onEndTimeChange: (time: string) => void;
+}
 
+export const TimePicker = ({
+  startTime,
+  endTime,
+  onStartTimeChange,
+  onEndTimeChange,
+}: TimePickerProps) => {
   return (
     <TimeWrapper>
-      <TimeInput type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+      <TimeInput
+        type="time"
+        value={startTime}
+        onChange={(e) => onStartTimeChange(e.target.value)}
+      />
       <TimeLabel>부터</TimeLabel>
-      <TimeInput type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+      <TimeInput type="time" value={endTime} onChange={(e) => onEndTimeChange(e.target.value)} />
       <TimeLabel>까지 방문</TimeLabel>
     </TimeWrapper>
   );
