0f77535b6ce63d25d1fa4acec110792d3479a76a
solmee <kii5541@naver.com>
2024-11-03 01:22:47 +0900

    feat: space페이지 구현

---
+++ src/pages/SpacePage/SpacePage.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  max-width: 43rem;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+  margin: 0 auto;
+  background-color: white;
+`;
--- src/pages/SpacePage/SpacePage.tsx
+++ src/pages/SpacePage/SpacePage.tsx
@@ -1,3 +1,34 @@
+import { useRef } from 'react';
+
+import MainImage from '@/assets/SpacePage/mainimage.svg';
+import MainImage2 from '@/assets/SpacePage/mainimage2.svg';
+import MainImage3 from '@/assets/SpacePage/mainimage3.svg';
+import { BottomSheet } from '@/components/BottomSheet/BottomSheet';
+import { BackgroundImage } from '@/components/Carousel/Carousel';
+
+import { Container } from './SpacePage.style';
+
 export const SpacePage = () => {
-  return <div>공간 상세 페이지</div>;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const images = [MainImage, MainImage2, MainImage3, MainImage];
+
+  const spaceInfo = {
+    title: 'eea',
+    location: '서울특별시 동작구 369',
+    category: '실내 공간',
+    outlet: '적음',
+    size: '작음',
+    wifi: '없음',
+    noise: '낮음',
+    mood1: '🎆 편안한',
+    mood2: '🪑 아늑한',
+  };
+
+  return (
+    <Container ref={containerRef}>
+      <BackgroundImage images={images} />
+      <BottomSheet spaceInfo={spaceInfo} />
+    </Container>
+  );
 };
