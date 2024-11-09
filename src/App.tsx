5b4a61204da849d4ec6ac60fb5e5a1c1e4cf012f
solmee <kii5541@naver.com>
2024-11-09 22:14:21 +0900

    feat: AppWrapper를 생성하여 메인 페이지들만 nav 적용 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,56 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
 
 import './App.css';
+import { BottomNav } from './components/BottomNav/BottomNav';
 import { BookSearchPage } from './pages/BookSearchPage/BookSearchPage';
 import { ChatPage } from './pages/ChatPage/ChatPage';
-import { ReportPlacePage } from './pages/ReportPlacePage/ReportPlacePage';
-import { MyPlacePage } from './pages/MyPlacePage/MyPlacePage';
 import { FilterPage } from './pages/FilterPage/FilterPage';
+import { MyPlacePage } from './pages/MyPlacePage/MyPlacePage';
+import { ReportPlacePage } from './pages/ReportPlacePage/ReportPlacePage';
 import { ReviewPage } from './pages/ReviewPage/ReviewPage';
 import { Home } from './pages/home/home';
 import { Mypage } from './pages/mypage/mypage';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/Theme';
 
+const AppWrapper = styled.div<{ $hasNavbar: boolean }>`
+  ${({ $hasNavbar }) =>
+    $hasNavbar &&
+    `
+   padding-bottom: 70px;
+ `}
+`;
+
+const AppContent = () => {
+  const location = useLocation();
+  const showNavbarPaths = ['/', '/myplace', '/mypage'];
+  const showNavbar = showNavbarPaths.includes(location.pathname);
+
+  return (
+    <AppWrapper $hasNavbar={showNavbar}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/chatpage" element={<ChatPage />} />
+        <Route path="/myplace" element={<MyPlacePage />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="/booksearch" element={<BookSearchPage />} />
+        <Route path="/reportplace" element={<ReportPlacePage />} />
+        <Route path="/filter" element={<FilterPage />} />
+      </Routes>
+      {showNavbar && <BottomNav />}
+    </AppWrapper>
+  );
+};
+
 export const App = () => {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/chatpage" element={<ChatPage />} />
-            <Route path="/myplace" element={<MyPlacePage />} />
-            <Route path="/review" element={<ReviewPage />} />
-            <Route path="/booksearch" element={<BookSearchPage />} />
-            <Route path="/reportplace" element={<ReportPlacePage />} />
-            <Route path="/filter" element={<FilterPage />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
