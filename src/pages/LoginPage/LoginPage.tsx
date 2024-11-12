3ad2b06cab6a62be5de05c64ace8671ddc3f8cad
solmee <kii5541@naver.com>
2024-11-12 16:50:25 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,19 @@
+import kakao from '@/assets/login/kakao.svg';
+import logo from '@/assets/login/logo.svg';
+
+import { Container, KakaoButton, LoginSubText, LoginText, LogoImage } from './LoginPage.style';
+
 export const LoginPage = () => {
-  return <div>로그인 페이지</div>;
+  const handleKakaoLogin = () => {
+    console.log('카카오 로그인 시도');
+  };
+
+  return (
+    <Container>
+      <LogoImage src={logo} alt="Bookmark 로고" />
+      <LoginText>Bookmark</LoginText>
+      <LoginSubText>매일매일 새로운 독서 공간 추천</LoginSubText>
+      <KakaoButton onClick={handleKakaoLogin} src={kakao} alt="카카오로 로그인하기" />
+    </Container>
+  );
 };
