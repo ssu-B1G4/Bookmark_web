73760b9965df5887e2ec12822609563ca35b6224
solmee <kii5541@naver.com>
2024-11-14 02:20:10 +0900

    feat: 토큰을 관리하는 TokenService 기능 구현

---
+++ src/service/TokenService.ts
@@ -0,0 +1,37 @@
+import { Cookies } from 'react-cookie';
+
+class TokenService {
+  private cookie = new Cookies();
+
+  setAccessToken(token: string) {
+    this.cookie.set('accessToken', token, {
+      path: '/',
+      secure: true,
+      sameSite: 'strict',
+    });
+  }
+
+  getAccessToken() {
+    return this.cookie.get('accessToken');
+  }
+
+  setRefreshToken(token: string) {
+    this.cookie.set('refreshToken', token, {
+      path: '/',
+      secure: true,
+      sameSite: 'strict',
+    });
+  }
+
+  getRefreshToken() {
+    return this.cookie.get('refreshToken');
+  }
+
+  logout() {
+    this.cookie.remove('accessToken', { path: '/' });
+    this.cookie.remove('refreshToken', { path: '/' });
+    sessionStorage.removeItem('user');
+  }
+}
+
+export const api = new TokenService();
