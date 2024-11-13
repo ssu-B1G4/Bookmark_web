c4114aa93c41135222cdd1e555f632ffa6766dc5
solmee <kii5541@naver.com>
2024-11-14 01:21:57 +0900

    feat: 로그인 페이지 상수 분리

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,9 +3,13 @@ import logo from '@/assets/login/logo.svg';
 
 import { Container, KakaoButton, LoginSubText, LoginText, LogoImage } from './LoginPage.style';
 
+const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+
 export const LoginPage = () => {
   const handleKakaoLogin = () => {
-    console.log('카카오 로그인 시도');
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   return (
