6f17e28a7f58bb9f5fad4c0c9ea757215ac880ae
solmee <kii5541@naver.com>
2024-11-21 01:57:27 +0900

    feat: sentry init 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import './index.css';
 import './styles/fonts.css';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', import.meta.env.VITE_API_BASE_URL],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
