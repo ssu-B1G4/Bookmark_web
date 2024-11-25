64ff8af0c76830edc4d80408bff0c29044cc8025
solmee <kii5541@naver.com>
2024-11-25 18:04:03 +0900

    setting: sentry 플러그인 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'soongsil-university-uz',
+      project: 'javascript-react',
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
 });
