ab57e95cb5fb68a50d7b5692077cdadce9c486c2
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-12-05 06:10:12 +0900

    fix: BottomSheet 컴포넌트 prop 추가

--- src/components/BottomSheet/BottomSheet.style.ts
+++ src/components/BottomSheet/BottomSheet.style.ts
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion';
 import { styled } from 'styled-components';
 
-export const StyledBottomSheet = styled(motion.div)<{ height: number }>`
+export const StyledBottomSheet = styled(motion.div)<{ height: number; $zIndex: number }>`
+  display: flex;
+  flex-direction: column;
   position: absolute;
   bottom: 0;
   left: 0;
@@ -12,8 +14,11 @@ export const StyledBottomSheet = styled(motion.div)<{ height: number }>`
   border-top-left-radius: 16px;
   border-top-right-radius: 16px;
   overflow: visible;
+  z-index: ${({ $zIndex }) => $zIndex || 0};
 `;
 
 export const ContentContainer = styled.div`
+  flex: 1;
   padding-top: 24px;
+  overflow: hidden;
 `;
