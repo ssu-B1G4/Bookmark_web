546f223a0431f96e3221d08d23723cbb3b226327
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-12-05 07:21:31 +0900

    delete: SearchResultBottomSheetPage 삭제

--- src/pages/home/SearchResultBottomSheet/SearchResultBottomSheetPageProps.ts
+++
@@ -1,7 +0,0 @@
-import { PlacePreviewDTO } from '@/types/Place';
-
-export type SearchResultBottomSheetPageProps = {
-  places: PlacePreviewDTO[];
-  isLastPage: boolean;
-  loaderRef: React.RefObject<HTMLDivElement>;
-};
