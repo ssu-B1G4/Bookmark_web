de4aafea4499d4d5aed818233b772a6fdd41ce81
solmee <kii5541@naver.com>
2024-12-05 16:41:30 +0900

    feat: 이미지 압축 전송 기능 구현

--- src/apis/postReportPlace.ts
+++ src/apis/postReportPlace.ts
@@ -1,3 +1,5 @@
+import imageCompression from 'browser-image-compression';
+
 import { ReportFormData } from '@/types/ReviewPage/ReviewFormData';
 import { client } from '@/utils/axios';
 import { transformReportData } from '@/utils/enumTransforms';
@@ -5,12 +7,28 @@ import { transformReportData } from '@/utils/enumTransforms';
 export const postReportPlace = async (postdata: ReportFormData, images?: File[]) => {
   const formData = new FormData();
   const transformedData = transformReportData(postdata);
-  console.log(postdata);
 
   formData.append('reportData', JSON.stringify(transformedData));
 
   if (images && images.length > 0) {
-    images.forEach((image) => {
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
+
+    const compressedImages = await Promise.all(
+      images.map(async (image) => {
+        try {
+          return await imageCompression(image, options);
+        } catch (error) {
+          console.error('이미지 압축 실패:', error);
+          return image;
+        }
+      })
+    );
+
+    compressedImages.forEach((image) => {
       formData.append('images', image);
     });
   }
