0e7c9a52ee8481195e93e4ca6a197e0cb0b9dd1c
solmee <kii5541@naver.com>
2024-12-05 15:53:06 +0900

    feat: 전역 bookmark 컴포넌트 생성

---
+++ src/components/Bookmark/Bookmark.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const IconButton = styled.button`
+  width: 34px;
+  height: 34px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
