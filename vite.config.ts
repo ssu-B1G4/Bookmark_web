919b4c83115feaad16ee27190a5793b2de93307d
solmee <kii5541@naver.com>
2024-12-07 21:52:00 +0900

    feat: 번들 최적화 적용

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          animations: ['framer-motion'],
+          styling: ['styled-components'],
+          queries: ['@tanstack/react-query'],
+        },
+      },
+    },
+  },
 });
