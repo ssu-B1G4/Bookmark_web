0de24e6ae981cc755a9c5a5e12e2e0ac9a28e719
solmee <kii5541@naver.com>
2024-12-09 06:58:23 +0900

    feat: 채팅 관련 타입 및 api 작성

---
+++ src/types/chat.ts
@@ -0,0 +1,18 @@
+export interface ChatMessageResponse {
+  nickname: string;
+  message: string;
+  timestamp: string;
+}
+
+export interface ApiResponse<T> {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: T;
+}
+
+export interface SendMessageRequest {
+  nickname: string;
+  message: string;
+  timestamp: string;
+}
