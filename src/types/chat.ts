fde708fd70abdf58534125a2c9b3935e2e8f6413
solmee <kii5541@naver.com>
2024-12-09 15:46:10 +0900

    fix: 타입 주석 해제

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,21 +1,21 @@
-// export interface ChatMessageResponse {
-//   nickname: string;
-//   message: string;
-//   timestamp: string;
-// }
+export interface ChatMessageResponse {
+  nickname: string;
+  message: string;
+  timestamp: string;
+}
 
-// export interface ApiResponse<T> {
-//   isSuccess: boolean;
-//   code: string;
-//   message: string;
-//   result: T;
-// }
+export interface ApiResponse<T> {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: T;
+}
 
-// export interface SendMessageRequest {
-//   nickname: string;
-//   message: string;
-//   timestamp: string;
-// }
+export interface SendMessageRequest {
+  nickname: string;
+  message: string;
+  timestamp: string;
+}
 
 export interface ChatMessageDTO {
   nickname: string;
